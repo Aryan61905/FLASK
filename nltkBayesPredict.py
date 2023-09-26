b203909873030fba4ca339046f7fb0bfefa94296
FinnThompson <Finn.thompson@Gmail.com>
2023-09-26 19:01:20 -0400

    Improving model accuracy for bayes and logistic

--- nltkBayesPredict.py
+++ nltkBayesPredict.py
@@ -2,15 +2,11 @@ import nltk
 import re
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
 from collections import Counter
 import numpy as np
-
 
 # Step 1: Read and tokenize the data
 data = []
@@ -28,7 +24,7 @@ pos_tags = [pos_tag for _, pos_tag in data]
 # Step 2: Create a list of previous POS tags
 prev_pos_tags = ["<START>"] + pos_tags[:-1]
 
-# Step 3: Extract capitalization, prefixes, and suffixes as features
+# Step 3: Extract features using NLTK
 features = []
 for i in range(len(words)):
     current_word = words[i]
@@ -50,6 +46,7 @@ for i in range(len(words)):
     suffix1 = current_word[-1:]
     suffix2 = current_word[-2:]
     suffix3 = current_word[-3:]
+    
     # Word length
     word_length = len(current_word)
 
@@ -59,57 +56,33 @@ for i in range(len(words)):
     # Presence of punctuation
     has_punctuation = int(any(char in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" for char in current_word))
 
-    # Word frequency
-    word_frequency = Counter[current_word]
-
-    # POS tags of previous two words
-    prev_prev_pos = prev_pos_tags[i - 2] if i >= 2 else "<START>"
-    prev_pos = prev_pos_tags[i - 1] if i >= 1 else "<START>"
-
-    # Combine features
-    feature = f"{current_word} {previous_word} {next_word} {is_capitalized} {prefix1} {prefix2} {prefix3} {suffix1} {suffix2} {suffix3} {word_length} {has_digits} {has_punctuation}"
-    features.append(feature)
+    # Combine features as a dictionary
+    feature = {
+        'word': current_word,
+        'prev_word': previous_word,
+        'next_word': next_word,
+        'is_capitalized': is_capitalized,
+        'prefix1': prefix1,
+        'prefix2': prefix2,
+        'prefix3': prefix3,
+        'suffix1': suffix1,
+        'suffix2': suffix2,
+        'suffix3': suffix3,
+        'word_length': word_length,
+        'has_digits': has_digits,
+        'has_punctuation': has_punctuation,
+    }
+    features.append((feature, current_pos))
 
 # Step 4: Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features, pos_tags, test_size=0.2, random_state=42)
 
-# Step 5: Vectorize the features using CountVectorizer
-vectorizer = CountVectorizer()
-X_train_vectorized = vectorizer.fit_transform(X_train)
-X_test_vectorized = vectorizer.transform(X_test)
+# Step 5: Define NLTK classifiers and train them
+from nltk.classify import NaiveBayesClassifier
 
-# Step 6: Train models to predict POS tags
-# Multinomial Naive Bayes
-nb_classifier = MultinomialNB()
-nb_classifier.fit(X_train_vectorized, y_train)
-y_pred_nb = nb_classifier.predict(X_test_vectorized)
-accuracy_nb = accuracy_score(y_test, y_pred_nb)
-print("Multinomial Naive Bayes Accuracy:", accuracy_nb)
-
-# Logistic Regression
-logistic_classifier = LogisticRegression(max_iter=1000)
-logistic_classifier.fit(X_train_vectorized, y_train)
-y_pred_logistic = logistic_classifier.predict(X_test_vectorized)
-accuracy_logistic = accuracy_score(y_test, y_pred_logistic)
-print("Logistic Regression Accuracy:", accuracy_logistic)
-
-# Support Vector Machine (SVM)
-# Add feature scaling with StandardScaler
-#scaler = StandardScaler()
-#X_train_scaled = scaler.fit_transform(X_train_vectorized.toarray())  # Convert to dense array for scaling
-#X_test_scaled = scaler.transform(X_test_vectorized.toarray())
-
-#svm_classifier = SVC(max_iter=200)
-#svm_classifier.fit(X_train_scaled, y_train)
-#y_pred_svm = svm_classifier.predict(X_test_scaled)
-#accuracy_svm = accuracy_score(y_test, y_pred_svm)
-#print("SVM Accuracy:", accuracy_svm)
-#svm_classifier = SVC()
-#svm_classifier.fit(X_train_scaled, y_train)
-#y_pred_svm = svm_classifier.predict(X_test_scaled)
-#accuracy_svm = accuracy_score(y_test, y_pred_svm)
-#print("SVM Accuracy:", accuracy_svm)
-
+# Naive Bayes Classifier
+nb_classifier = NaiveBayesClassifier.train(X_train)
+print("Naive Bayes Classifier Accuracy:", nltk.classify.accuracy(nb_classifier, X_test))
 
 print("here")
 # Step 1: Read and tokenize the test data
@@ -122,7 +95,6 @@ with open('test.txt', 'r') as file:
             token, pos_tag, _ = line.split()  # Extract POS tag from test data
             test_data.append(token)
             test_pos_tags.append(pos_tag)
-
 
 # Step 2: Extract features from the test data
 print("1")
@@ -155,29 +127,30 @@ for i in range(len(test_data)):
     # Presence of punctuation
     has_punctuation = int(any(char in "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" for char in current_word))
 
-    # Combine features
-    feature = f"{current_word} {previous_word} {next_word} {is_capitalized} {prefix1} {prefix2} {prefix3} {suffix1} {suffix2} {suffix3} {word_length} {has_digits} {has_punctuation}"
+    # Combine features as a dictionary
+    feature = {
+        'word': current_word,
+        'prev_word': previous_word,
+        'next_word': next_word,
+        'is_capitalized': is_capitalized,
+        'prefix1': prefix1,
+        'prefix2': prefix2,
+        'prefix3': prefix3,
+        'suffix1': suffix1,
+        'suffix2': suffix2,
+        'suffix3': suffix3,
+        'word_length': word_length,
+        'has_digits': has_digits,
+        'has_punctuation': has_punctuation,
+    }
     test_features.append(feature)
 
-    # Step 3: Vectorize the test features using the same CountVectorizer
-X_test_vectorized = vectorizer.transform(test_features)
-
-# Step 4: Use your trained models to make predictions on the test data
+# Step 3: Use NLTK classifiers to make predictions on the test data
 print("3")
-y_pred_nb = nb_classifier.predict(X_test_vectorized)
-y_pred_logistic = logistic_classifier.predict(X_test_vectorized)
+y_pred_nb = [nb_classifier.classify(feature) for feature in test_features]
 
-# Standardize test data for SVM
-#X_test_scaled = scaler.transform(X_test_vectorized.toarray())  # Convert to dense array for scaling
-#y_pred_svm = svm_classifier.predict(X_test_scaled)
-
-# Step 5: Evaluate the performance of your models
+# Step 4: Evaluate the performance of your models
 # You can calculate accuracy by comparing the predicted POS tags with the actual POS tags in test_pos_tags.
 
 accuracy_nb = accuracy_score(test_pos_tags, y_pred_nb)
-accuracy_logistic = accuracy_score(test_pos_tags, y_pred_logistic)
-#accuracy_svm = accuracy_score(test_pos_tags, y_pred_svm)
-
-print("Multinomial Naive Bayes Accuracy:", accuracy_nb)
-print("Logistic Regression Accuracy:", accuracy_logistic)
-#print("SVM Accuracy:", accuracy_svm)
+print("Naive Bayes Classifier Accuracy:", accuracy_nb)
